056729c7550682f76045d69c345d86f1f0dc2e4a
melq <tu2sice@yahoo.co.jp>
2021-02-11 23:12:28 +0900

    :lollipop: 各要素への id の付加

--- app/src/main/java/com/melq/morse/MainActivity.kt
+++ app/src/main/java/com/melq/morse/MainActivity.kt
@@ -3,12 +3,12 @@ package com.melq.morse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
 }

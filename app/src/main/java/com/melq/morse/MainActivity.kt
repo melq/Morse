a1a5e4f7cd4729e0476e8844172095e509409b69
melq <tu2sice@yahoo.co.jp>
2021-02-11 18:58:56 +0900

    :art: input エリアの見た目を作成

--- app/src/main/java/com/melq/morse/MainActivity.kt
+++ app/src/main/java/com/melq/morse/MainActivity.kt
@@ -2,10 +2,13 @@ package com.melq.morse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }

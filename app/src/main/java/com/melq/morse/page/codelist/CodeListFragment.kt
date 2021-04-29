7c4b5fca08563c602c3ee394548508341c7f18f2
melq <tu2sice@yahoo.co.jp>
2021-04-29 21:59:55 +0900

    :construction: コード表と履歴のフラグメントとレイアウトファイルを作成

---
+++ app/src/main/java/com/melq/morse/page/codelist/CodeListFragment.kt
@@ -0,0 +1,7 @@
+package com.melq.morse.page.codelist
+
+import androidx.fragment.app.Fragment
+import com.melq.morse.R
+
+class CodeListFragment : Fragment(R.layout.fragment_code_list) {
+}
---
+++ app/src/main/java/com/melq/morse/page/history/HistoryFragment.kt
@@ -0,0 +1,7 @@
+package com.melq.morse.page.history
+
+import androidx.fragment.app.Fragment
+import com.melq.morse.R
+
+class HistoryFragment : Fragment(R.layout.fragment_history) {
+}
